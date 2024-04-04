1295ac392b7457c9a424d30856ca2e90fa1462f0
Shiva Dhakal <theskdhakal@gmail.com>
2024-04-04 16:47:12 +1100

    property card built

--- src/app/properties/add/page.tsx
+++ src/app/properties/add/page.tsx
@@ -1,7 +1,56 @@
 import React from "react";
+import properties from "@/properties.json";
+import PropertyCard from "@/components/PropertyCard";
+
+export interface Property {
+  _id: string;
+  owner: string;
+  name: string;
+  type: string;
+  description: string;
+  location: {
+    street: string;
+    city: string;
+    state: string;
+    zipcode: string;
+  };
+  beds: number;
+  baths: number;
+  square_feet: number;
+  amenities: string[];
+  rates: {
+    nightly?: number;
+    weekly?: number;
+    monthly?: number;
+  };
+  seller_info: {
+    name: string;
+    email: string;
+    phone: string;
+  };
+  images: string[];
+  is_featured: boolean;
+  createdAt: string;
+  updatedAt: string;
+}
 
 const PropertiesAddPage = () => {
-  return <div>PropertiesAddPage</div>;
+  return (
+    //  < !--All Listings-- >
+    <section className="px-4 py-6">
+      <div className="container-xl lg:container m-auto px-4 py-6">
+        {properties.length === 0 ? (
+          <p>No properties found</p>
+        ) : (
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            {properties.map((property: Property) => (
+              <PropertyCard key={property._id} property={property} />
+            ))}
+          </div>
+        )}
+      </div>
+    </section>
+  );
 };
 
 export default PropertiesAddPage;
