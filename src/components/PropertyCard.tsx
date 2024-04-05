9fa3d916a7c178f6458fb3c733653a23e813c039
Shiva Dhakal <theskdhakal@gmail.com>
2024-04-06 10:32:48 +1100

    property card made dynamic

--- src/components/PropertyCard.tsx
+++ src/components/PropertyCard.tsx
@@ -1,49 +1,74 @@
 import { Property } from "@/app/properties/add/page";
 import React from "react";
+import Image from "next/image";
+import Link from "next/link";
 
 interface Props {
   property: Property;
 }
 
 const PropertyCard: React.FC<Props> = ({ property }) => {
+  const { rates } = property;
+  const getRateDisplay = () => {
+    if (rates.monthly) {
+      return `${rates.monthly.toLocaleString()}/mo`;
+    } else if (rates.weekly) {
+      return `${rates.weekly.toLocaleString()}/wk`;
+    } else if (rates.nightly) {
+      return `${rates.nightly.toLocaleString()}/night`;
+    }
+  };
+
   return (
     <div className="rounded-xl shadow-md relative">
-      <img
-        src="images/properties/a1.jpg"
+      <Image
+        src={`/images/properties/${property.images[0]}`}
         alt=""
+        height={0}
+        width={0}
+        sizes="100vw"
         className="w-full h-auto rounded-t-xl"
       />
       <div className="p-4">
         <div className="text-left md:text-center lg:text-left mb-6">
-          <div className="text-gray-600">Apartment</div>
-          <h3 className="text-xl font-bold">Boston Commons Retreat</h3>
+          <div className="text-gray-600">{property.type}</div>
+          <h3 className="text-xl font-bold">{property.name}</h3>
         </div>
         <h3 className="absolute top-[10px] right-[10px] bg-white px-4 py-2 rounded-lg text-blue-500 font-bold text-right md:text-center lg:text-right">
-          $4,200/mo
+          ${getRateDisplay()}
         </h3>
 
         <div className="flex justify-center gap-4 text-gray-500 mb-4">
           <p>
-            <i className="fa-solid fa-bed"></i> 3
-            <span className="md:hidden lg:inline">Beds</span>
+            <i className="fa-solid fa-bed"></i> {property.beds}{" "}
+            <span className=" lg:inline">Beds</span>
           </p>
           <p>
-            <i className="fa-solid fa-bath"></i> 2
-            <span className="md:hidden lg:inline">Baths</span>
+            <i className="fa-solid fa-bath"></i> {property.baths}{" "}
+            <span className=" lg:inline">Baths</span>
           </p>
           <p>
             <i className="fa-solid fa-ruler-combined"></i>
-            1,500 <span className="md:hidden lg:inline">sqft</span>
+            {property.square_feet} <span className=" lg:inline">sqft</span>
           </p>
         </div>
 
         <div className="flex justify-center gap-4 text-green-900 text-sm mb-4">
-          <p>
-            <i className="fa-solid fa-money-bill"></i> Weekly
-          </p>
-          <p>
-            <i className="fa-solid fa-money-bill"></i> Monthly
-          </p>
+          {rates.nightly && (
+            <p>
+              <i className="fa-solid fa-money-bill"></i> Nightly
+            </p>
+          )}
+          {rates.weekly && (
+            <p>
+              <i className="fa-solid fa-money-bill"></i> Weekly
+            </p>
+          )}
+          {rates.monthly && (
+            <p>
+              <i className="fa-solid fa-money-bill"></i> Monthly
+            </p>
+          )}
         </div>
 
         <div className="border border-gray-100 mb-5"></div>
@@ -51,10 +76,15 @@ const PropertyCard: React.FC<Props> = ({ property }) => {
         <div className="flex flex-col lg:flex-row justify-between mb-4">
           <div className="flex align-middle gap-2 mb-4 lg:mb-0">
             <i className="fa-solid fa-location-dot text-lg text-orange-700"></i>
-            <span className="text-orange-700"> Boston MA </span>
+            <span className="text-orange-700">
+              {" "}
+              {property.location.city}
+              {property.location.state}
+              {""}{" "}
+            </span>
           </div>
           <a
-            href="property.html"
+            href={`/properties/${property._id}`}
             className="h-[36px] bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-lg text-center text-sm"
           >
             Details
