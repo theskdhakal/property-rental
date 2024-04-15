dca21a1c25e7366eb4c168a6770ea02a74e9ff1a
Shiva Dhakal <theskdhakal@gmail.com>
2024-04-15 14:02:46 +1000

    custom tailwind added on config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,13 +8,15 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontFamily: {
+        sans: ["Poppins", "sans-serif"],
+      },
+
+      gridTemplateColumns: {
+        "70/30": "70% 28%",
       },
     },
+    plugins: [],
   },
-  plugins: [],
 };
 export default config;
