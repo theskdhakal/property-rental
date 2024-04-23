4e2e1577922d77364b78d21ab291f559f461d1ca
Shiva Dhakal <theskdhakal@gmail.com>
2024-04-23 14:02:01 +1000

    property saved to db

---
+++ src/utils/getSessionUser.ts
@@ -0,0 +1,32 @@
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "@/utils/authOptions";
+import { DefaultSession } from "next-auth";
+
+interface ExtendedSessionUser extends DefaultSession {
+  user?: {
+    name?: string | null;
+    email?: string | null;
+    image?: string | null;
+    id: string;
+  };
+}
+
+export const getSessionUser = async () => {
+  try {
+    const session: ExtendedSessionUser | null = await getServerSession(
+      authOptions
+    );
+
+    if (!session || !session.user) {
+      return null;
+    }
+
+    return {
+      user: session.user,
+      userId: session.user.id,
+    };
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+};
