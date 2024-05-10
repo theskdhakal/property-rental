ce42407f65979a442bc52de45c509c2dc39bddc5
Shiva Dhakal <theskdhakal@gmail.com>
2024-05-10 22:18:24 +1000

    react image swipe deployed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import Footer from "@/components/Footer";
 import AuthProvider from "@/components/AuthProvider";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import "photoswipe/dist/photoswipe.css";
 import { GlobalProvider } from "@/context/GlobalContext";
 
 export const metadata = {
